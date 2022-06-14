90280da551a6f55ecd8ce0bc287f10bb936a527e
Geordi <guoxji00@futurMaster.com>
2022-06-14 10:39:42 +0800

    init handsonTableDemo

---
+++ handsontableDemo/src/constants.ts
@@ -0,0 +1,124 @@
+export const data =  [
+  [
+    false,
+    "Tagcat",
+    "United Kingdom",
+    "Classic Vest",
+    "11/10/2020",
+    "01-2331942",
+    true,
+    "172",
+    2,
+    2
+  ],
+  [
+    true,
+    "Zoomzone",
+    "Indonesia",
+    "Cycling Cap",
+    "03/05/2020",
+    "88-2768633",
+    true,
+    "188",
+    6,
+    2
+  ],  [
+    true,
+    "Meeveo",
+    "United States",
+    "Full-Finger Gloves",
+    "27/03/2020",
+    "51-6775945",
+    true,
+    "162",
+    1,
+    3
+  ],
+  [
+    false,
+    "Buzzdog",
+    "Philippines",
+    "HL Mountain Frame",
+    "29/08/2020",
+    "44-4028109",
+    true,
+    "133",
+    7,
+    1
+  ],
+  [
+    true,
+    "Katz",
+    "India",
+    "Half-Finger Gloves",
+    "02/10/2020",
+    "08-2758492",
+    true,
+    "87",
+    1,
+    3
+  ],
+  [
+    false,
+    "Jaxbean",
+    "China",
+    "HL Road Frame",
+    "28/09/2020",
+    "84-3557705",
+    false,
+    "26",
+    8,
+    1
+  ],
+  [
+    false,
+    "Jaxbean",
+    "China",
+    "HL Road Frame",
+    "28/09/2020",
+    "84-3557705",
+    false,
+    "26",
+    8,
+    1
+  ],
+  [
+    false,
+    "Jaxbean",
+    "China",
+    "HL Road Frame",
+    "28/09/2020",
+    "84-3557705",
+    false,
+    "26",
+    8,
+    1
+  ],
+  [
+    false,
+    "Jaxbean",
+    "China",
+    "HL Road Frame",
+    "28/09/2020",
+    "84-3557705",
+    false,
+    "26",
+    8,
+    1
+  ],
+  [
+    false,
+    "Wikido",
+    "Brazil",
+    "HL Touring Frame",
+    "24/06/2020",
+    "20-9397637",
+    false,
+    "110",
+    4,
+    1
+  ]]
+
+export const SELECTED_CLASS = "selected";
+export const ODD_ROW_CLASS = "odd";
+
