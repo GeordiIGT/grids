489fae77b784471ab4ea0268f8dd93edb9b348d2
Geordi <jia.guo@futurmaster.com>
2022-06-20 10:38:21 +0800

    antd table with group element using children node.

--- antd-grid-ts/src/App.tsx
+++ antd-grid-ts/src/App.tsx
@@ -1,7 +1,7 @@
 import { DownOutlined } from '@ant-design/icons';
-import React, { useCallback, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Badge, Dropdown, Menu, Space, Table, Button } from 'antd';
-import getData from './data';
+import getData, {resetCounter} from './data';
 const menu = (
   <Menu items={[
     { key: '1', label: 'Action 1', },
@@ -11,69 +11,55 @@ const menu = (
 const columns = [
   { key: 'label', dataIndex: 'label', title: 'Label' },
   { key: 'percentage', dataIndex: 'percentage', title: 'Percentage %', headerTooltip: "Percentage %", minWidth: 150, editable: true, },
-  { key: 'p1', dataIndex: 'p1' },
-  { key: 'p2', dataIndex: 'p2' },
-  { key: 'p3', dataIndex: 'p3' },
-  { key: 'p4', dataIndex: 'p4' },
-  { key: 'p5', dataIndex: 'p5' },
-  { key: 'p6', dataIndex: 'p6' },
+  { key: 'p1', dataIndex: 'p1', title: 'p1' },
+  { key: 'p2', dataIndex: 'p2', title: 'p2' },
+  { key: 'p3', dataIndex: 'p3', title: 'p3' },
+  { key: 'p4', dataIndex: 'p4', title: 'p4' },
+  { key: 'p5', dataIndex: 'p5', title: 'p5' },
+  { key: 'p6', dataIndex: 'p6', title: 'p6' },
 ];
 
 const timer = { timeStart: 0, timeGDDuration: 0, timeEnd: 0 };
 
 const App = () => {
-  const [rowData, setRowData] = useState(getData(5).rs);
-  const expandedRowRender = (item: any) => {
-    const mycolumns = [
-      { key: 'label', dataIndex: 'label', title: 'Label' },
-      { key: 'percentage', dataIndex: 'percentage', title: 'Percentage %', headerTooltip: "Percentage %", minWidth: 150, editable: true, },
-      { key: 'p1', dataIndex: 'p1' },
-      { key: 'p2', dataIndex: 'p2' },
-      { key: 'p3', dataIndex: 'p3' },
-      { key: 'p4', dataIndex: 'p4' },
-      { key: 'p5', dataIndex: 'p5' },
-      { key: 'p6', dataIndex: 'p6' },
-    ];
-    const data = getData(12).rs;
-    function insertMultiTab(num: number) {
-      let rt = "";
-      if (num > 1) {
-        for (let i = 0; i < Math.ceil(num); i++) {
-          rt += "/t";
-        }
-      }
-      return rt;
-    }
-    return <Table columns={mycolumns} dataSource={data} pagination={false} showHeader={false} />;
-  };
-  function onChangeHandler(arg:any){
+  const data = getData(5).rs;
+  resetCounter();
+  const [rowData, setRowData] = useState(data);
+  function onChangeHandler(arg: any) {
     debugger;
   }
 
   function onUpdateRecords(numOfRecords: number) {
     console.log("Start: " + numOfRecords + " rows. *************************************************************************************************************");
-    // console.log("Button clicked, start generating " + numOfRecords + " rows of data.");
     timer.timeStart = new Date().getTime();
+    resetCounter();
     const { rs, genDuration } = getData(numOfRecords);
     timer.timeGDDuration = genDuration;
     console.log("Data generating completed\nTime cost: " + timer.timeGDDuration + " ms, new data length is " + rs.length);
     setRowData(rs);
   }
-  const data = getData(50).rs;
+  useEffect(() => {
+    timer.timeEnd = new Date().getTime();
+    if (timer.timeStart !== 0) {
+      // console.log("Render completed:"+evt.rowData.currentValue.length+" rows now\n"+evt.rowData.previousValue.length+" rows previously.");
+      console.log((timer.timeEnd - timer.timeStart) + " milliseconds past.**************************************************************************************");
+    }
+  }, [rowData]);
   return (
     <div>
       {/* <Button style={{ marginBottom: '5px', fontWeight: 'bold' }} onClick={onBtnExportDataAsExcel} >Export to Excel({numOfData} records)</Button> */}
       <Button style={{ marginLeft: '15px' }} onClick={() => { onUpdateRecords(50); }}>50 rows</Button>
       <Button style={{ marginLeft: '15px' }} onClick={() => { onUpdateRecords(200); }}>200 rows</Button>
+      <Button style={{ marginLeft: '15px' }} onClick={() => { onUpdateRecords(1000); }}>1000 rows</Button>
       <Button style={{ marginLeft: '15px' }} onClick={() => { onUpdateRecords(10000); }}>10,000 rows</Button>
       <Button style={{ marginLeft: '15px' }} onClick={() => { onUpdateRecords(100000); }}>100,000 rows</Button>
 
       <Table
         className="components-table-demo-nested"
         columns={columns}
-        expandable={{
-          expandedRowRender,
-        }}
+        // expandable={{
+        //   expandedRowRender,
+        // }}
         dataSource={rowData}
         pagination={false}
       />
--- antd-grid-ts/src/data.ts
+++ antd-grid-ts/src/data.ts
@@ -1,34 +1,47 @@
+let counter = 0;
+export const resetCounter = ()=>{
+    counter = 0;
+};
 function getData(numOfRows: number) {
     const time1 = new Date().getTime();
     const sample = genANodeData();
-    numOfRows = (numOfRows && numOfRows > sample.length) ? Math.ceil(numOfRows) : sample.length;
-    const numOfLoops = Math.ceil(numOfRows / sample.length);
+    numOfRows = (numOfRows && numOfRows > 7) ? Math.ceil(numOfRows) : 7;
+    const numOfLoops = Math.ceil(numOfRows / 7);
     let rs: any = [];
     let counter = 0;
     for (let i = 0; i < numOfLoops; i++) {
         for (const item of sample) {
             // if (item.label.length > 1) {
-            const newLabel = item.label.map((name: string) => name + "_" + i);
-            rs.push({
-                key: i + "" + counter,
-                label: newLabel,
-                p1: item.p1,
-                p2: item.p2,
-                p3: item.p3,
-                p4: item.p4,
-                p5: item.p5,
-                p6: item.p6,
-                rowColour: (item.rowColour) ? item.rowColour : "",
-                rowColourIndex: item.rowColourIndex,
-                'percentage': random(100) + "%"
-            });
-            counter++;
-            // }
+            rs.push(formatObjRecursively(item, i));
         }
     }
     const time2 = new Date().getTime();
     // console.log("Data generating cost: " + (time2 - time1) + " milliseconds");
     return { "rs": rs, "genDuration": (time2 - time1) };
+}
+
+function formatObjRecursively(obj: any, surfix: number) {
+    const newLabel = obj.label.map((name: string) => name + "_" + counter);
+    const rt = {
+        key: surfix + "_" + counter++,
+        label: newLabel.pop(),
+        p1: obj.p1,
+        p2: obj.p2,
+        p3: obj.p3,
+        p4: obj.p4,
+        p5: obj.p5,
+        p6: obj.p6,
+        rowColour: (obj.rowColour) ? obj.rowColour : "",
+        rowColourIndex: obj.rowColourIndex,
+        'percentage': random(100) + "%",
+        children: [],
+    };
+
+    if (obj.children && obj.children.length !== 0) {//has children
+        const children = obj.children.map((item: any) => formatObjRecursively(item, surfix));
+        if(children !== undefined) rt.children = children;
+    }
+    return rt;
 }
 
 function random(max: number = 100, min: number = 0): number {
@@ -47,74 +60,82 @@ function genANodeData(): any[] {
             p6: "P6",
             rowColour: "#ffcc66",
             rowColourIndex: 0,
-        },
-        {
-            label: ["Node", 'SOH'],
-            p1: 500,
-            p2: 800,
-            p3: 800,
-            p4: "",
-            p5: "",
-            p6: "",
-            rowColour: "#ffcccc",
-            rowColourIndex: 1
-        },
-        {
-            label: ["Node", 'SOH', 'Gross Req'],
-            p1: 200,
-            p2: 300,
-            p3: 200,
-            p4: 300,
-            p5: 200,
-            p6: 100,
-            rowColour: "#ffcccc",
-            rowColourIndex: 1
-        },
-        {
-            label: ["Node", 'SOH', 'Stock Coverage'],
-            p1: 2,
-            p2: 3,
-            p3: 4,
-            p4: '',
-            p5: '',
-            p6: '',
-            rowColour: "#ffcccc",
-            rowColourIndex: 1
-        },
-        {
-            label: ["Node", 'Hist 2000'],
-            p1: 500,
-            p2: 700,
-            p3: 800,
-            p4: 900,
-            p5: 200,
-            p6: 300,
-            rowColour: "#ccccff",
-            rowColourIndex: 2
-        },
-        {
-            label: ["Node", 'Hist 2000', 'Hist 2001'],
-            p1: 700,
-            p2: 800,
-            p3: 900,
-            p4: 500,
-            p5: 678,
-            p6: 235,
-            rowColour: "#ccccff",
-            rowColourIndex: 2
-        },
-        {
-            label: ["Node", 'Hist 2000', 'Hist 2002'],
-            p1: 565,
-            p2: 5457,
-            p3: 455,
-            p4: 676,
-            p5: 245,
-            p6: 467,
-            rowColour: "#ccccff",
-            rowColourIndex: 2
+            children: [
+                {
+                    label: ["Node", 'SOH'],
+                    p1: 500,
+                    p2: 800,
+                    p3: 800,
+                    p4: "",
+                    p5: "",
+                    p6: "",
+                    rowColour: "#ffcccc",
+                    rowColourIndex: 1,
+                    children: [
+                        {
+                            label: ["Node", 'SOH', 'Gross Req'],
+                            p1: 200,
+                            p2: 300,
+                            p3: 200,
+                            p4: 300,
+                            p5: 200,
+                            p6: 100,
+                            rowColour: "#ffcccc",
+                            rowColourIndex: 1
+                        },
+                        {
+                            label: ["Node", 'SOH', 'Stock Coverage'],
+                            p1: 2,
+                            p2: 3,
+                            p3: 4,
+                            p4: '',
+                            p5: '',
+                            p6: '',
+                            rowColour: "#ffcccc",
+                            rowColourIndex: 1
+                        },
+                    ]
+                },
+                {
+                    label: ["Node", 'Hist 2000'],
+                    p1: 500,
+                    p2: 700,
+                    p3: 800,
+                    p4: 900,
+                    p5: 200,
+                    p6: 300,
+                    rowColour: "#ccccff",
+                    rowColourIndex: 2,
+                    children: [
+                        {
+                            label: ["Node", 'Hist 2000', 'Hist 2001'],
+                            p1: 700,
+                            p2: 800,
+                            p3: 900,
+                            p4: 500,
+                            p5: 678,
+                            p6: 235,
+                            rowColour: "#ccccff",
+                            rowColourIndex: 2
+                        },
+                        {
+                            label: ["Node", 'Hist 2000', 'Hist 2002'],
+                            p1: 565,
+                            p2: 5457,
+                            p3: 455,
+                            p4: 676,
+                            p5: 245,
+                            p6: 467,
+                            rowColour: "#ccccff",
+                            rowColourIndex: 2
+                        },
+                    ],
+                },
+            ],
         },
     ];
     return rowData;
 }
+
+
 export default getData;
